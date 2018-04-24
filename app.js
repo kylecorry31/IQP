bace55ec4ad005403e64b20b13f17f9b6e6f6ef7
Kyle Corry <kylecorry31@gmail.com>
2018-04-24 07:44:47 -0400

    Allow selection of iqp from list

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+var iqps = [
+  {
+    project: "Sustainability Compilation",
+    location: {
+      name: "Worcester Polytechnic Institute, Worcester, MA, USA",
+      latitude: 42.2746,
+      longitude: -71.8063
+    },
+    authors: ["Ema Mehuljic", "Kyle Corry"],
+    advisors: ["Prof. Shockey", "Prof. Rosenstock", "Prof. Mathisen"],
+    type: "IQP",
+    year: "2018 - 2019",
+    description: "Creating a way for students to learn more and become part of the sustainability community of Worcester and WPI's IQPs.",
+    url: "https://www.wpi.edu"
+  },
+  {
+    project: "Something Else",
+    location: {
+      name: "Chepachet, RI, USA",
+      latitude: 41.9152,
+      longitude: -71.6714
+    },
+    authors: ["Kyle Corry"],
+    advisors: ["Nobody"],
+    type: "IQP",
+    year: "2017 - 2018",
+    description: "Description here",
+    url: "https://kylecorry31.github.io"
+  }
+];
+
 var Globe = function(ArcGIS, containerID, center, zoom){
 
   var container = document.getElementById(containerID);
@@ -145,7 +176,11 @@ require([
 
       globe.addLayer(csvLayer);
 
-      var qrcode = new QRCodeManager(document.getElementById("detail-qrcode"), "http://www.wpi.edu");
+    });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+        var qrcode = new QRCodeManager(document.getElementById("detail-qrcode"), "http://www.wpi.edu");
 
       document.getElementById("globeToggle").checked = true;
       document.getElementById("kioskToggle").checked = false;
@@ -169,6 +204,27 @@ require([
         document.getElementById("iqp-url").innerHTML = url;
       });
 
+      EventBus.subscribe("iqp", displayIQP);
+
+
+      var iqpList = document.getElementById("iqp-list");
+      var iqpString = "";
+      var count = 0;
+      iqps.forEach(function(iqp){
+        var li = '<li class="mdl-list__item mdl-js-ripple-effect mdl-list__item--three-line" onclick="displayIQP(iqps['+count+']);">\
+          <span class="mdl-ripple"></span>\
+          <span class="mdl-list__item-primary-content">\
+            <span>' + iqp.project +'</span>\
+          <span class="mdl-list__item-text-body">' +
+          iqp.location.name +
+          '</span>\
+          </span>\
+        </li>';
+        iqpString += li;
+        count++;
+      });
+
+      iqpList.innerHTML = iqpString;
     });
 
 function kioskStateChanged(){
@@ -179,4 +235,29 @@ function kioskStateChanged(){
 function displayGlobeStateChanged(){
   var sw = document.getElementById("globeToggle");
   EventBus.publish("globe-visible", sw.checked);
+}
+
+function displayIQP(iqp){
+
+  var coords = {
+    latitude: iqp.location.latitude,
+    longitude: iqp.location.longitude,
+    zoom: 8
+  };
+
+  EventBus.publish("globe-center", coords);
+  EventBus.publish("iqp-url", iqp.url);
+  document.getElementById("detail-table").classList.remove("hidden");
+  document.getElementById("iqp-list").classList.add("hidden");
+  document.getElementById("detail-title").innerHTML = iqp.project;
+
+  var table = document.getElementById("detail-table");
+
+  table.rows[0].cells[1].innerHTML = iqp.location.name;
+  table.rows[1].cells[1].innerHTML = iqp.authors.join(", ");
+  table.rows[2].cells[1].innerHTML = iqp.advisors.join(", ");
+  table.rows[3].cells[1].innerHTML = iqp.type;
+  table.rows[4].cells[1].innerHTML = iqp.year;
+  table.rows[5].cells[1].innerHTML = iqp.description;
+
 }
