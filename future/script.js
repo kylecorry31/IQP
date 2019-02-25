797b8d99119ba5f4490974cf9f511d197760ea67
Kyle Corry <kylecorry31@gmail.com>
2019-02-25 11:15:37 -0500

    Adjust size

--- future/script.js
+++ future/script.js
@@ -25,7 +25,7 @@ document.body.addEventListener('keypress', function(event){
     }
   } else if (event.key == 'q'){
     window.history.back();
-  }  
+  }
 });
 
 let img;
@@ -42,21 +42,21 @@ function fortune(string, length){
   var max_on_time = random(400, 800);
   var max_off_time = max_on_time;
   this.next_transition = Math.floor(random(0, max_off_time));
-  
-	
+
+
 	this.nextPos = function(){
 		var angle = map(noise(this.wordAng), 0, 1, 0, PI * 4);
 		var magnitude = map(noise(this.wordMag), 0, 1, 10, this.length);
 
 		var x = cos(angle) * magnitude;
 		var y = sin(angle) * magnitude;
-		
+
 		this.wordAng += 0.002;
 		this.wordMag += 0.005;
-		
+
 		return [x, y];
 	}
-	
+
 	this.draw = function(){
     if(this.next_transition <= 0){
 
@@ -79,12 +79,12 @@ function fortune(string, length){
     var pos = this.nextPos(this.length);
     fill(255, 255, 255, Math.floor(255 * this.opacity));
     textAlign(CENTER, CENTER);
-    textSize(24);
+    textSize(32);
     textFont("Harry P");
-    text(this.string, pos[0] + width/2, pos[1] + 100);
+    text(this.string, pos[0] + width/2, pos[1] + 150);
     this.next_transition--;
 	}
-	
+
 }
 
 function clip(value, min, max){
@@ -96,15 +96,15 @@ function preload() {
 }
 
 function setup() {
-  let myCanvas = createCanvas(400, 400);
+  let myCanvas = createCanvas(600, 600);
   myCanvas.parent('ball');
 	wordAng = 0;
 	wordMag = 10;
-	words.push(new fortune('IQP', 80));
-	words.push(new fortune('Classes', 50));
-	words.push(new fortune('Training', 40));
-	words.push(new fortune('Display', 40));
-	words.push(new fortune('Guide', 40));
+	words.push(new fortune('IQP', 100));
+	words.push(new fortune('Classes', 90));
+	words.push(new fortune('Training', 80));
+	words.push(new fortune('Display', 80));
+	words.push(new fortune('Guide', 80));
 }
 
 function draw() {
